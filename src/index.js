f5230927e169148cc642664d7f542bad7025a4ed
Kayla Cammack <kaylacammack@gmail.com>
2024-05-20 22:09:35 -0600

    Rendering Lists

--- src/index.js
+++ src/index.js
@@ -75,8 +75,15 @@ function Header() {
 function Menu() {
 	return (
 		<main className="menu">
-			<h3>Our menu</h3>
-			<Pizza
+			<h2>Our menu</h2>
+
+			<ul className="pizzas">
+				{pizzaData.map((pizza) => (
+					<Pizza pizzaObj={pizza} key={pizza.name} />
+				))}
+			</ul>
+
+			{/* <Pizza
 				name="Pizza Spinaci"
 				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
 				photoName="pizzas/spinaci.jpg"
@@ -88,7 +95,7 @@ function Menu() {
 				ingredients="Tomato, mushrooms"
 				price={12}
 				photoName="pizzas/funghi.jpg"
-			/>
+			/> */}
 		</main>
 	);
 }
@@ -97,14 +104,14 @@ function Pizza(props) {
 	console.log(props);
 
 	return (
-		<div className="pizza">
-			<img src={props.photoName} alt={props.name} />
+		<li className="pizza">
+			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
 			<div>
-				<h3>{props.name}</h3>
-				<p>{props.ingredients}</p>
-				<span>{props.price + 3}</span>
+				<h3>{props.pizzaObj.name}</h3>
+				<p>{props.pizzaObj.ingredients}</p>
+				<span>{props.pizzaObj.price + 3}</span>
 			</div>
-		</div>
+		</li>
 	);
 }
 
