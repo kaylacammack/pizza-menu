bf284eb143ea16924073e28fde1b29ab0b4f0ee7
Kayla Cammack <kaylacammack@gmail.com>
2024-05-20 21:36:01 -0600

    Passing & Receiving Props

--- src/index.js
+++ src/index.js
@@ -76,11 +76,35 @@ function Menu() {
 	return (
 		<main className="menu">
 			<h3>Our menu</h3>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+			<Pizza
+				name="Pizza Spinaci"
+				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+				photoName="pizzas/spinaci.jpg"
+				price={10}
+			/>
+
+			<Pizza
+				name="Pizza Funghi"
+				ingredients="Tomato, mushrooms"
+				price={12}
+				photoName="pizzas/funghi.jpg"
+			/>
 		</main>
+	);
+}
+
+function Pizza(props) {
+	console.log(props);
+
+	return (
+		<div className="pizza">
+			<img src={props.photoName} alt={props.name} />
+			<div>
+				<h3>{props.name}</h3>
+				<p>{props.ingredients}</p>
+				<span>{props.price + 3}</span>
+			</div>
+		</div>
 	);
 }
 
@@ -103,16 +127,6 @@ function Footer() {
 	);
 
 	// return React.createElement("footer", null, "We're currently open!");
-}
-
-function Pizza() {
-	return (
-		<div>
-			<img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-			<h2>Pizza Spinaci</h2>
-			<p>Tomato, mozarella, spinach, and ricotta cheese</p>
-		</div>
-	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
