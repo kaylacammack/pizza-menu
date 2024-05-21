346c6423de7de60c69ab698f43e87d5e6ba1904a
Kayla Cammack <kaylacammack@gmail.com>
2024-05-21 09:03:55 -0600

    Conditional Rendering With Ternaries

--- src/index.js
+++ src/index.js
@@ -81,12 +81,16 @@ function Menu() {
 		<main className="menu">
 			<h2>Our menu</h2>
 
-			{numPizzas > 0 && (
+			{numPizzas > 0 ? (
 				<ul className="pizzas">
 					{pizzas.map((pizza) => (
 						<Pizza pizzaObj={pizza} key={pizza.name} />
 					))}
 				</ul>
+			) : (
+				<p>
+					We're still working on our menu. Please come back later :)
+				</p>
 			)}
 
 			{/* <Pizza
@@ -135,7 +139,7 @@ function Footer() {
 
 	return (
 		<footer className="footer">
-			{isOpen && (
+			{isOpen ? (
 				<div className="order">
 					<p>
 						We're open until {closeHour}:00. Come visit us or order
@@ -143,6 +147,11 @@ function Footer() {
 					</p>
 					<button className="btn">Order</button>
 				</div>
+			) : (
+				<p>
+					We're happy to welcome you between {openHour}:00 and{" "}
+					{closeHour}:00.
+				</p>
 			)}
 		</footer>
 	);
